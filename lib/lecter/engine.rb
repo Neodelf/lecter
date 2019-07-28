dff1920701d46eea70f102236eb1bdc0cacaa883
Neodelf <neodelf@gmail.com>
2019-07-28 21:56:42 +0300

    Base for comfortable UI

--- lib/lecter/engine.rb
+++ lib/lecter/engine.rb
@@ -1,5 +1,8 @@
 module Lecter
   class Engine < ::Rails::Engine
     isolate_namespace Lecter
+    config.before_initialize do
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    end
   end
 end
