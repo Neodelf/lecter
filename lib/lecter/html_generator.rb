a0a3b2c0d5d95fa98e4a0de75e7571f40575cdef
Neodelf <neodelf@gmail.com>
2020-03-30 20:02:00 +0300

    Refactor

--- lib/lecter/html_generator.rb
+++ lib/lecter/html_generator.rb
@@ -3,7 +3,8 @@ require 'lecter/html_row'
 module Lecter
   class HtmlGenerator
     COUNT_LINES_AROUND_RUNNING_ROW = 5.freeze
-    ELLIPSIS = "...\n".freeze
+    ELLIPSIS = "...".freeze
+    NEW_LINE = "\n".freeze
 
     def initialize(data)
       @data = data
@@ -12,11 +13,11 @@ module Lecter
     def call
       @data.each_with_index.map do |item, item_index|
         @file_name = item.keys.first
+
         @executable_row_numbers = item.values.split(' ').flatten.map(&:to_i)
         previous_row_is_empty = false
-        file_context = File.open(Rails.root.join(file_name), 'r').read.split("\n")
 
-        @html_rows = file_context.each_with_index.map do |file_row, file_row_index|
+        html_rows = file_context.each_with_index.map do |file_row, file_row_index|
           @file_row_index = file_row_index
           row_executable = executable_row_numbers.include?(file_row_index + 1)
 
@@ -29,7 +30,7 @@ module Lecter
               executable_row_numbers).create
           elsif !previous_row_is_empty
             previous_row_is_empty = true
-            ELLIPSIS
+            ELLIPSIS + NEW_LINE
           end
         end
 
@@ -39,14 +40,18 @@ module Lecter
 
     private
 
-    attr_accessor :executable_row_numbers, :file_row_index, :file_name, :html_rows
+    attr_accessor :executable_row_numbers, :file_row_index, :file_name
 
     def file_row_in_showing_range?(index)
-      executable_row_numbers.reduce(false) do |memo, line_index|
+      executable_row_numbers.reduce(false) do |memo, row_number|
         memo || file_row_index.in?(
-          line_index - COUNT_LINES_AROUND_RUNNING_ROW - 1..
-            line_index + COUNT_LINES_AROUND_RUNNING_ROW - 1)
+          row_number - COUNT_LINES_AROUND_RUNNING_ROW - 1..
+            row_number + COUNT_LINES_AROUND_RUNNING_ROW - 1)
       end
+    end
+
+    def file_context
+      File.open(Rails.root.join(file_name), 'r').read.split(NEW_LINE)
     end
   end
 
